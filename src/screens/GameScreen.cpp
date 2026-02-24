995af8d9b3ed0de1648f2247324df6960701e6ce
Marco <fallace.marco@gmail.com>
2026-02-24 13:51:20 +0100

    Fixed Clock not showing, it works now

--- src/screens/GameScreen.cpp
+++ src/screens/GameScreen.cpp
@@ -5,20 +5,21 @@ class Game;
 
 GameScreen::GameScreen(Game &game)
     : FloweryScreen(game),
-        Quit(FloweryButton(sf::RectangleShape({200.f, 100.f}), sf::Text(ResourcesHandler.BaseFont, "Back to Menu"), sf::Color(255, 0, 0, 255))),
-        ClockUI(sf::Text(ResourcesHandler.GameCounterFont, ""))
+        Quit(FloweryButton(sf::RectangleShape({200.f, 100.f}), sf::Text(ResourcesHandler.BaseFont, "Menu"), sf::Color(255, 0, 0, 255))),
+        ClockUI(sf::Text(ResourcesHandler.BaseFont, "clock"))
     {
         sf::Vector2f WindowCenter = {game.GetWindowSize().x/2, game.GetWindowSize().y/2};
-        Quit.setPosition({WindowCenter.x - Quit.getSize().x/2, WindowCenter.y - 50});
+        Quit.setPosition({static_cast<float>(WindowCenter.x * 1.5 - 10), static_cast<float>(10)});
         
         Quit.MB1action = [&game](){ game.BackToMenuAction(); };
         Quit.MB2action = [](){};
 
-        ClockUI.setPosition({0.f, 0.f});
+        ClockUI.setPosition({10.f, 10.f});
     }
 
 void GameScreen::Draw() {
     this->game.drawFloweryButton(this->Quit);
+    this->game.drawLabel(this->ClockUI);
 }
 
 void GameScreen::OnMB1(sf::RenderWindow &GameWindow) {
